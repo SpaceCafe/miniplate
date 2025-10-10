ea45aa668aa91c6fff9bb72bd13ba9eb1a9c6841
Lars Thoms <lars@thoms.io>
2025-10-10 13:08:57 +0200

    ✨ implement LoadContexts with file, stdin, and web support

--- pkg/context.go
+++ pkg/context.go
@@ -2,6 +2,9 @@ package pkg
 
 import (
 	"fmt"
+	"io"
+	"net/http"
+	"net/url"
 	"os"
 	"path"
 	"strings"
@@ -9,37 +12,126 @@ import (
 	"github.com/SpaceCafe/miniplate/pkg/functions"
 )
 
-func LoadContexts(files []string) (ctx map[string]any, err error) {
+func LoadContexts(ctxList []string) (ctx map[string]any, err error) {
 	ctx = make(map[string]any)
-	dataFuncs := &functions.DataFuncs{}
-	for _, file := range files {
+	for _, ctxItem := range ctxList {
 		var (
-			rawData []byte
-			data    map[string]any
+			ctxURL *url.URL
+			data   map[string]any
 		)
 
-		parts := strings.SplitN(file, "=", 2)
-		if len(parts) != 2 || parts[0] == "" || parts[1] == "" {
-			return ctx, fmt.Errorf("invalid file format: %s", file)
+		ctxName, ctxRawURL, ok := strings.Cut(ctxItem, "=")
+		if !ok || (ctxName == "" && ctxRawURL == "") {
+			return ctx, fmt.Errorf("unsupported context format: %s", ctxItem)
 		}
 
-		filePath := path.Clean(parts[1])
-		rawData, err = os.ReadFile(path.Clean(filePath))
+		ctxURL, err = url.Parse(ctxRawURL)
 		if err != nil {
-			return
+			return nil, err
 		}
-		switch path.Ext(filePath) {
-		case ".json":
-			data, err = dataFuncs.JSON(rawData)
-		case ".toml":
-			data, err = dataFuncs.TOML(rawData)
-		case ".yaml":
-			data, err = dataFuncs.YAML(rawData)
+
+		switch ctxURL.Scheme {
+		case "stdin":
+			data, err = loadContextFromStdin(ctxURL)
+		case "file", "":
+			data, err = loadContextFromFile(ctxURL)
+		case "http", "https":
+			data, err = loadContextFromWeb(ctxURL)
+		default:
+			return nil, fmt.Errorf("unsupported context schema: %s", ctxURL.Scheme)
 		}
 		if err != nil {
-			return nil, fmt.Errorf("failed to parse file %s: %w", filePath, err)
+			return nil, err
 		}
-		ctx[parts[0]] = data
+		ctx[ctxName] = data
+	}
+	return
+}
+
+func loadContextFromStdin(ctxURL *url.URL) (data map[string]any, err error) {
+	var (
+		buf []byte
+	)
+
+	buf, err = io.ReadAll(os.Stdin)
+	if err != nil {
+		return
+	}
+
+	return parseContext(ctxURL, buf)
+}
+
+func loadContextFromFile(ctxURL *url.URL) (data map[string]any, err error) {
+	var (
+		buf []byte
+	)
+
+	filePath := path.Clean(ctxURL.Host + ctxURL.Path)
+	buf, err = os.ReadFile(path.Clean(filePath))
+	if err != nil {
+		return
+	}
+
+	return parseContext(ctxURL, buf)
+}
+
+func loadContextFromWeb(ctxURL *url.URL) (data map[string]any, err error) {
+	var (
+		buf []byte
+	)
+
+	resp, err := http.Get(ctxURL.String())
+	if err != nil {
+		return
+	}
+	defer func() { _ = resp.Body.Close() }()
+	buf, err = io.ReadAll(resp.Body)
+	if err != nil {
+		return
+	}
+	if resp.StatusCode != http.StatusOK {
+		return nil, fmt.Errorf("failed to load context at %s: %s", ctxURL.String(), resp.Status)
+	}
+	ctxURL.RawQuery = "type=" + resp.Header.Get("Content-Type")
+
+	return parseContext(ctxURL, buf)
+}
+
+func parseContext(ctxURL *url.URL, buf []byte) (data map[string]any, err error) {
+	var (
+		dataFuncs = &functions.DataFuncs{}
+	)
+
+	if len(buf) == 0 {
+		return nil, fmt.Errorf("empty context at %s", ctxURL.String())
+	}
+
+	switch path.Ext(ctxURL.Path) {
+	case ".json":
+		data, err = dataFuncs.JSON(buf)
+	case ".toml":
+		data, err = dataFuncs.TOML(buf)
+	case ".yaml":
+		data, err = dataFuncs.YAML(buf)
+	default:
+		mime := ctxURL.Query().Get("type")
+		if mime == "" {
+			mime, _, _ = strings.Cut(http.DetectContentType(buf), ";")
+		}
+
+		switch mime {
+		case "application/json":
+			data, err = dataFuncs.JSON(buf)
+		case "application/toml":
+			data, err = dataFuncs.TOML(buf)
+		case "application/yaml", "text/plain":
+			data, err = dataFuncs.YAML(buf)
+		default:
+			return nil, fmt.Errorf("unsupported context mime type at %s: %s", ctxURL.String(), mime)
+		}
+	}
+	if err != nil {
+		return nil, fmt.Errorf("failed to parse context at %s: %w", ctxURL.String(), err)
 	}
 	return
 }
---
+++ pkg/context_test.go
@@ -0,0 +1,55 @@
+package pkg
+
+import (
+	"fmt"
+	"reflect"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestLoadContexts(t *testing.T) {
+	tests := []struct {
+		name    string
+		input   []string
+		want    map[string]any
+		wantErr bool
+	}{
+		{"file", []string{"user1=file://../test/testdata.json"}, map[string]any{"firstname": "John", "lastname": "Doe"}, false},
+		{"web", []string{"user1=https://jsonplaceholder.typicode.com/users/1"}, map[string]any{
+			"id":       1,
+			"name":     "Leanne Graham",
+			"username": "Bret",
+			"email":    "Sincere@april.biz",
+			"address": map[string]any{
+				"street":  "Kulas Light",
+				"suite":   "Apt. 556",
+				"city":    "Gwenborough",
+				"zipcode": "92998-3874",
+				"geo": map[string]any{
+					"lat": "-37.3159",
+					"lng": "81.1496",
+				},
+			},
+			"phone":   "1-770-736-8031 x56442",
+			"website": "hildegard.org",
+			"company": map[string]any{
+				"name":        "Romaguera-Crona",
+				"catchPhrase": "Multi-layered client-server neural-net",
+				"bs":          "harness real-time e-markets",
+			},
+		}, false},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			got, err := LoadContexts(tt.input)
+			if tt.wantErr {
+				assert.Error(t, err)
+			} else {
+				assert.NoError(t, err)
+				fmt.Printf("%+v\n", got)
+				reflect.DeepEqual(got, tt.want)
+			}
+		})
+	}
+}
